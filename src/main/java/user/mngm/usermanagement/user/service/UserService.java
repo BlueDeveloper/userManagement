802de7551091f0d40f59a2508ee8e941efb9a615
blueehdwp <blueehdwp@naver.com>
2023-02-23 09:15:21 +0900

    security 관련 설정 파일 생성 유저관련 파일 생성 exception 처리 파일 생성 mvc 관련 설정 파일 생성 로그인 시 ajax 요청 관리 파일 생성

---
+++ src/main/java/user/mngm/usermanagement/user/service/UserService.java
@@ -0,0 +1,10 @@
+package user.mngm.usermanagement.user.entity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// TODO 유저정보 저장하는 로직 구현
+public class UserService {
+}
