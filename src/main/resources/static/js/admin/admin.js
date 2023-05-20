31f9ec63e7c5a3824632666de42ed766f6558b6c
blueehdwp <ehd@129323>
2023-05-20 15:21:50 +0900

    고도화 작업-2 1. html파일과 1:1 대응되는 js 파일 생성 및 script 코드 이전

---
+++ src/main/resources/static/js/admin/admin.js
@@ -0,0 +1,66 @@
+const gbl = {}
+$(function () {
+    init()                                                          // 초기화
+    $(".searchBtn").on("click", beforeSearchFromBtn)
+})
+
+function init() {
+    search()
+    leftMenu();
+}
+
+function leftMenu() {
+    const menu = searchParam("menu");
+    $(".leftMenu").removeClass("active");
+    (menu === null) ? $(".leftMenu.tab-1").addClass("active") : $(".leftMenu.tab-" + menu).addClass("active");
+}
+
+function search(data) {
+    data = (data === undefined) ? {"page": 0, "size": 5} : data
+    ajaxCall("POST", "/api/admin/userList", data, true, null,
+        (data) => {
+            let userHtml = ``
+            $.each(data.data.content, (idx, target) => {
+                userHtml += `<div class='usersDiv num${idx}'>
+                                            <span>${target.name}</span><span>${target.memberId}</span>
+                                            <span>${target.email}</span><span>${target.loginDat}</span>
+                                            <span>
+                                                <select class="form-select w-u70 userStatSct" data-memberId="${target.memberId}">
+                                                    <option value="01" ${(target.stat === "활동") ? "selected" : ""}>활동</option>
+                                                    <option value="02" ${(target.stat === "정지") ? "selected" : ""}>정지</option>
+                                                    <option value="03" ${(target.stat === "탈퇴") ? "selected" : ""}>탈퇴</option>
+                                                </select>
+                                            </span>
+                                         </div>`
+            })
+            $(".body").html(userHtml); // 유저 리스트
+            $(".footer").html(calPageNum(data.data)) // 페이지 번호 세팅
+            $(".num").on("click", beforeSearchFromNums) // 페이지 이벤트 설정
+            $(".userStatSct").on("change", chagneUserStat) // 페이지 이벤트 설정
+
+        }, null)
+}
+
+function beforeSearchFromBtn(e) {
+    let obj = {"page": 0, "size": 5}
+    obj[$(".searchSct option:selected").val()] = $(".searchInp").val()
+    search(obj);
+}
+
+function beforeSearchFromNums(e) {
+    let obj = {"page": Number(e.target.dataset.num) - 1, "size": "5"}
+    obj[$(".searchSct option:selected").val()] = $(".searchInp").val()
+    search(obj);
+}
+
+function chagneUserStat(e) {
+    if (confirm("회원의 상태를 변경하시겠습니까?")) {
+        let data = {memberId: e.target.dataset.memberid, stat: e.target.value}
+        ajaxCall("POST", "/api/admin/changeUserStat", data, true, null,
+            (data) => {
+                let obj = {"page": Number($(".currentPage")[0].dataset.num) - 1, "size": "5",}
+                obj[$(".searchSct option:selected").val()] = $(".searchInp").val()
+                search(obj)
+            }, null)
+    }
+}
